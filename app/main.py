7780400986d8cb15eaeb3875168b992ac047955b
Ariel Martínez <ariel@martinezs.dev>
2021-08-20 11:26:26 -0400

    Attempt to fix production error

--- app/main.py
+++ app/main.py
@@ -8,14 +8,13 @@ from programs import example
 
 sentry_sdk.init(dsn=env.SENTRY_DSN, environment=env.STARLETTE_ENV)
 
-asgi_app = SentryAsgiMiddleware(app)
+# asgi_app = SentryAsgiMiddleware(app)
 
 
 programs = dict(example=example)
-
 
 @app.router("/", methods=["post"])
 def index(program_name, run_id):
     program = programs[program_name]
     program.exec(run_id)
-    return run_id
+    return "OK"
--- app/run.sh
+++ app/run.sh
@@ -3,5 +3,5 @@
 if [ "$RUN_ENV" = "development" ]; then
   exec asymmetric run --host 0.0.0.0 --port $PORT asgi --reload --log-level debug
 else
-  exec asymmetric run --host 0.0.0.0 --port $PORT asgi --log-level info
+  exec asymmetric run --host 0.0.0.0 --port $PORT asgi --log-level debug
 fi
