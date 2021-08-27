118589db84aa5b6dd48720f1bc9f57d5b523d7a9
Ariel Martínez <ariel@martinezs.dev>
2021-08-26 20:22:45 -0400

    Add Run model

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 
 import sentry_sdk
+from pydantic import BaseModel
 from sentry_sdk.integrations.asgi import SentryAsgiMiddleware
 
 import env
@@ -13,8 +14,13 @@ sentry_sdk.init(dsn=env.SENTRY_DSN, environment=env.FASTAPI_ENV)
 app.add_middleware(SentryAsgiMiddleware)
 
 
+class Run(BaseModel):
+    program_name: str
+    run_id: str
+
+
 @app.post("/")
-def index(program_name: str, run_id: str):
-    program = programs[program_name]
-    program.exec(run_id)
+def index(run: Run):
+    program = programs[run.program_name]
+    program.exec(run.run_id)
     return "OK"
