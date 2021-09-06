c6c7d74d3f61d7b8a9655a5cadfd9ba294008b12
Ariel Martínez <ariel@martinezs.dev>
2021-09-06 15:28:42 +0200

    Remove comment

--- app/programs/calculo_quinta_peru/__init__.py
+++ app/programs/calculo_quinta_peru/__init__.py
@@ -8,10 +8,6 @@ logger = logging.getLogger(__name__)
 
 
 def exec(run_id):
-    #########################
-    # Mount the file system #
-    #########################
-
     with FileSystem(run_id) as fs:
         main.run(fs)
         fs.commit_zip(dir="results")
