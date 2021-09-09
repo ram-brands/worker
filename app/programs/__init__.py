47eac5dea2e4bcfbb874cea7fc5bf93c8bf6af69
Alex Apt <alexaptib@gmail.com>
2021-09-09 19:50:47 -0300

    update: __init__.py

--- app/programs/__init__.py
+++ app/programs/__init__.py
@@ -1,3 +1,3 @@
-from . import calculo_quinta_peru, example
+from . import calculo_quinta_peru, devoluciones_api_mercado_pago, example
 
-programs = dict(calculo_quinta_peru=calculo_quinta_peru, example=example)
+programs = dict(calculo_quinta_peru=calculo_quinta_peru, devoluciones_api_mercado_pago=devoluciones_api_mercado_pago, example=example)
