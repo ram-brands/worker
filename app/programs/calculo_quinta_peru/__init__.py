c76e8caf754b43a9a58e45282dcadc0792a75fde
Ariel Martínez <ariel@martinezs.dev>
2021-09-10 15:28:09 +0200

    Fix typo

--- app/programs/calculo_quinta_peru/__init__.py
+++ app/programs/calculo_quinta_peru/__init__.py
@@ -5,9 +5,5 @@ from . import main
 
 def exec(run_id):
     with FileSystem(run_id) as fs:
-        import time
-
-        time.sleep(30)
-
         main.run(fs)
         fs.commit_zip(dir="results")
