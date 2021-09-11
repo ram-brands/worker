e976f2c8dfc0838872d0237fe8ed8cefd81a5588
Ariel Martínez <ariel@martinezs.dev>
2021-09-11 10:40:34 +0200

    Remove sleep

--- app/programs/calculo_quinta_peru/__init__.py
+++ app/programs/calculo_quinta_peru/__init__.py
@@ -5,8 +5,5 @@ from . import main
 
 def exec(run_id):
     with FileSystem(run_id) as fs:
-        import time
-
-        time.sleep(300)
         main.run(fs)
         fs.commit_zip(dir="results")
