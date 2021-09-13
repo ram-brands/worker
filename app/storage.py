50571496ba9e70854b9c9c54e1686f273454cfb8
Ariel Martínez <ariel@martinezs.dev>
2021-09-13 07:48:58 +0200

    Make boto3 client a singleton

--- app/storage.py
+++ app/storage.py
@@ -4,7 +4,17 @@ from botocore.client import Config as BotoConfig
 import env
 
 
-class Storage:
+class Singleton(type):
+    def __call__(cls, *args, **kwargs):
+        cls._instance = (
+            cls._instance
+            if hasattr(cls, "_instance")
+            else super().__call__(*args, **kwargs)
+        )
+        return cls._instance
+
+
+class Storage(metaclass=Singleton):
     bucket_name = env.RUNS_S3_BUCKET
 
     def __init__(self):
