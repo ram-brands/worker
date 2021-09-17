77a14686c90bfe39af695a2993019e94b4cc949f
Alex Apt <alexaptib@gmail.com>
2021-09-17 10:10:49 -0300

    format code

--- app/programs/reporte_de_inventario/modules/reader.py
+++ app/programs/reporte_de_inventario/modules/reader.py
@@ -1,5 +1,6 @@
 from collections import namedtuple
 from os import listdir
+
 from xlrd import open_workbook
 
 from . import paths
@@ -7,11 +8,15 @@ from . import paths
 
 def read_sap(_, store, keyword=paths.SAP_KEYWORD):  # Read sap stock [[sku, stock]]
     _.log(f"Leyendo sap stock...")
-    file_name = [f for f in listdir(_.get_path(f'data/{store}')) if ("~" not in f) and (keyword in f)]
+    file_name = [
+        f
+        for f in listdir(_.get_path(f"data/{store}"))
+        if ("~" not in f) and (keyword in f)
+    ]
     if file_name:
         file_name = file_name[0]
     else:
-        _.log(f'Incorrect format with store {store} and keyword {keyword}')
+        _.log(f"Incorrect format with store {store} and keyword {keyword}")
         raise FileNotFoundError
     wb = open_workbook(_.get_path(f"data/{store}/{file_name}"))
     for s in wb.sheets():
@@ -35,13 +40,19 @@ def read_sap(_, store, keyword=paths.SAP_KEYWORD):  # Read sap stock [[sku, stoc
     return data
 
 
-def read_physical(_, store, keyword=paths.PHYSICAL_KEYWORD):  # Read sap stock [[sku, stock]]
+def read_physical(
+    _, store, keyword=paths.PHYSICAL_KEYWORD
+):  # Read sap stock [[sku, stock]]
     _.log(f"Leyendo stock físico...")
-    file_name = [f for f in listdir(_.get_path(f'data/{store}')) if ("~" not in f) and (keyword in f)]
+    file_name = [
+        f
+        for f in listdir(_.get_path(f"data/{store}"))
+        if ("~" not in f) and (keyword in f)
+    ]
     if file_name:
         file_name = file_name[0]
     else:
-        _.log(f'Incorrect format with store {store} and keyword {keyword}')
+        _.log(f"Incorrect format with store {store} and keyword {keyword}")
         raise FileNotFoundError
     wb = open_workbook(_.get_path(f"data/{store}/{file_name}"))
     for s in wb.sheets():
@@ -62,11 +73,14 @@ def read_physical(_, store, keyword=paths.PHYSICAL_KEYWORD):  # Read sap stock [
                                 data[int(sku)] = 1
     return data
 
-def get_all_stores(_, dir_name='data'):
+
+def get_all_stores(_, dir_name="data"):
     # _.log('Get all stores')
     # print('get all stores')
     # all_stores = listdir(_.get_path(dir_name))
-    all_stores = [s for s in listdir(_.get_path(dir_name)) if ("~" not in s) and ("." not in s)]
+    all_stores = [
+        s for s in listdir(_.get_path(dir_name)) if ("~" not in s) and ("." not in s)
+    ]
     # print(all_stores)
     _.log(all_stores)
     return all_stores
